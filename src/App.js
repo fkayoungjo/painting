7ec11a80fcc91930f11075368fd7e247d304f016
fkayoungjo <Jordan.simon55@gmail.com>
2020-11-10 21:24:03 -0500

    colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Playground from './Playground.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Playground />
     </div>
   );
 }
