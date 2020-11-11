88f84c9abeb7001bc6a09667cdef01b362b9a4c1
fkayoungjo <Jordan.simon55@gmail.com>
2020-11-11 16:33:54 -0500

    colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { SketchPicker } from 'react-color';
 import Playground from './Playground.js'
 
 function App() {
   return (
     <div className="App">
       <Playground />
+      <SketchPicker />
     </div>
   );
 }
