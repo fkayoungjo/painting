b64ceb69f47ac6265c082d25e728fdff9dd6e3e8
fkayoungjo <Jordan.simon55@gmail.com>
2020-11-16 22:16:19 -0500

    back

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { SketchPicker } from 'react-color';
 import Playground from './Playground.js'
 
 function App() {
-  const canvasRef = useRef(null)
- const contextRef = useRef(null)
- const [isDrawing, setIsDrawing] = useState(false)
+  const [color, setColor] = useState('#000000');
 
- useEffect(() => {
-   const canvas = canvasRef.current;
-   canvas.width = window.innerWidth * 2;
-   canvas.height = window.innerHeight * 2;
-   canvas.style.width = `${window.innerWidth}px`;
-   canvas.style.height = `${window.innerHeight}px`;
+  function handleChangeComplete(color)  {
+    setColor(color.hex)
+  };
 
-   const context = canvas.getContext("2d")
-   context.scale(2,2)
-   context.lineCap = "round"
-   context.strokeStyle = "black"
-   context.lineWidth = 2
-   
-   contextRef.current = context;
- }, [])
-
- const startDrawing = ({nativeEvent}) => {
-   const {offsetX, offsetY} = nativeEvent;
-   contextRef.current.beginPath()
-   contextRef.current.moveTo(offsetX, offsetY)
-   setIsDrawing(true)
- }
-
- const finishDrawing = () => {
-  contextRef.current.closePath()
-  setIsDrawing(false)
- }
-
- const draw = ({nativeEvent}) => {
-   if(!isDrawing){
-     return
-   }
-   const {offsetX, offsetY} = nativeEvent;
-   contextRef.current.lineTo(offsetX, offsetY)
-   contextRef.current.stroke()
- }
-
- return (
-   <canvas
-     onMouseDown={startDrawing}
-     onMouseUp={finishDrawing}
-     onMouseMove={draw}
-     ref={canvasRef}
-   />
- );
+  return (
+    <div >
+      <SketchPicker color={ color }
+      onChangeComplete={ handleChangeComplete }/>
+    </div>
+  );
 }
+
 export default App;
